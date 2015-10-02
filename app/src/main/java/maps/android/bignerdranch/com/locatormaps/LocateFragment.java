3ec0fe2153eb3fc9dc6916f8ed6a6c35be88597b
almonick <almonick72@gmail.com>
2015-10-02 10:23:30 +0300

    google map zoom

--- app/src/main/java/maps/android/bignerdranch/com/locatormaps/LocateFragment.java
+++ app/src/main/java/maps/android/bignerdranch/com/locatormaps/LocateFragment.java
@@ -19,8 +19,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -122,10 +125,19 @@ public class LocateFragment extends SupportMapFragment {
 		LatLng itemPoint = new LatLng(mMapItem.getLat(), mMapItem.getLon());
 		LatLng myPoint = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
 
+		BitmapDescriptor itemBitmap = BitmapDescriptorFactory.fromBitmap(mMapImage);
+		MarkerOptions itemMarker = new MarkerOptions().position(itemPoint).icon(itemBitmap);
+		MarkerOptions myMarker = new MarkerOptions().position(myPoint);
+
+		mMap.clear();
+		mMap.addMarker(itemMarker);
+		mMap.addMarker(myMarker);
+
 		LatLngBounds bounds = new LatLngBounds.Builder().include(itemPoint).include(myPoint).build();
 		int margin = getResources().getDimensionPixelSize(R.dimen.map_inset_margin);
 		CameraUpdate update = CameraUpdateFactory.newLatLngBounds(bounds, margin);
 		mMap.animateCamera(update);
+
 
 
 
